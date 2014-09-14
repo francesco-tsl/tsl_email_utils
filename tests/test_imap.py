c45dbfba5644f9ce923af109acdec656b4c9b87c
Francesco Pischedda <francesco.pischedda@gmail.com>
2014-09-14 17:31:51 +0200

    changed tests to fit the new api, added test for the default_filter

--- tests/test_imap.py
+++ tests/test_imap.py
@@ -1,6 +1,5 @@
 import os
-from contact_email_parser import imap_download
-from contact_email_parser import parser
+from tsl_email_utils import imap_download
 
 
 def get_env_variable(var_name):
@@ -22,17 +21,24 @@ def test_imap_parse_messages():
                                           password, True)
 
     source = get_env_variable("EMAIL_SOURCE")
-    messages = m.fetch_messages(sent_from=source, unread=True)
+    messages = m.fetch_bodies_plain(sent_from=source)
 
     assert len(messages) > 0
+    assert messages[0] == "this is a message"
+
+
+def test_fetch_messages_with_default_filter():
+
+    host = get_env_variable("EMAIL_HOST")
+    username = get_env_variable("EMAIL_USERNAME")
+    password = get_env_variable("EMAIL_PASSWORD")
+    m = imap_download.EMailClient.connect(host,
+                                          username,
+                                          password, True)
+
+    source = get_env_variable("EMAIL_SOURCE")
+    func = imap_download.default_filter
+    messages = m.fetch_messages(func, False, sent_from=source)
 
     for m in messages:
-        print "original:", m
-        contact = parser.parse(m)
-        print "parsed:", contact
-        assert 'email' in contact
-        assert 'telefono' in contact
-        assert 'nome della compagnia' in contact
-        assert 'fatturato (reale o stimato)' in contact
-        assert 'servizio richiesto' in contact
-        assert 'details' in contact
+        assert len(m) == 2
